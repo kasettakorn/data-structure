1b19298782cfc9962a773cfd0e015c3c4720c2bc
Ronnakorn Hompoa <36933333+kasettakorn@users.noreply.github.com>
2024-05-02 16:58:59 +0700

    Update stack_array.cpp

--- stack/stack_array.cpp
+++ stack/stack_array.cpp
@@ -1,6 +1,67 @@
 #include <iostream>
 using namespace std;
 
+class Stack {
+    public:
+        int capacity;
+        int *stack;
+        int top;
+
+        Stack(int c) {
+            capacity = c;
+            stack = new int[c];
+            top = 0;
+        }
+
+        void push(int data) {
+            if (top == capacity) {
+                cout << "Stack is full." << endl;
+                return;
+            }
+            stack[top] = data;
+            top++;
+        }
+
+        void pop() {
+            if (top == 0) {
+                cout << "Stack is empty." << endl;
+                return;
+            }
+            int temp = top;
+            top--;
+        }
+
+        int peek() {
+            return stack[top-1];
+        }
+
+        void print() {
+            for (int i = 0; i < top; i++)
+            {
+                cout << stack[i] << " -> ";
+            }
+            cout << "_" << endl;
+        }
+};
+
+
 int main() {
+    Stack stack(3);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    stack.pop();
+    stack.pop();
+    stack.pop();
+
+    stack.push(4);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
     
+    stack.print();
+
+    cout << "Top of stack is " << stack.peek() << endl;
 }
